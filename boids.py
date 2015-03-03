cde49a185dadbe562fcd80dca2148324e43a89bb
joshualockhart <joshualockhart@gmail.com>
2015-03-03 20:06:21 +0000

    converted position update to use python

--- boids.py
+++ boids.py
@@ -15,36 +15,33 @@ boids_y=(300*np.random.random_sample((50,))+300)
 boid_x_velocities=(10*np.random.random_sample((50,)))
 boid_y_velocities=(40*np.random.random_sample((50,))-20)
 
-
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
-	xs,ys,xvs,yvs=boids
+  xs,ys,xvs,yvs=boids
   # Fly towards the middle
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
-	
+  for i in range(len(xs)):
+    for j in range(len(xs)):
+      xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
+  for i in range(len(xs)):
+    for j in range(len(xs)):
+      yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+  
   # Fly away from nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
-				xvs[i]=xvs[i]+(xs[i]-xs[j])
-				yvs[i]=yvs[i]+(ys[i]-ys[j])
-	# Try to match speed with nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
-	# Move according to velocities
-	for i in range(len(xs)):
-		xs[i]=xs[i]+xvs[i]
-		ys[i]=ys[i]+yvs[i]
-
+  for i in range(len(xs)):
+    for j in range(len(xs)):
+      if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
+        xvs[i]=xvs[i]+(xs[i]-xs[j])
+        yvs[i]=yvs[i]+(ys[i]-ys[j])
+  # Try to match speed with nearby boids
+  for i in range(len(xs)):
+    for j in range(len(xs)):
+      if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
+        xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
+        yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+  
+  xs+=xvs
+  ys+=yvs
 
 figure=plt.figure()
 axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
